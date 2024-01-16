142322086fff7254ec5b51c2c557096a90d55596
Olena Havuliak <olena.havuliak@nerdysoft.com>
2024-01-16 21:19:07 +0200

    Initial commit

---
+++ src/main/java/com/example/finnybuddy/FinnyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.finnybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnyBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finnybuddy/FinnyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finnybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
