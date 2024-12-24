ef532ea7304383db950c134f054d452e31708ae0
olenahavuliak <gavuliak1@gmail.com>
2024-12-24 13:05:42 +0200

    refactor

--- src/main/java/com/example/finnybuddy/domain/user/repository/UserEntityRepository.java
+++ src/main/java/com/example/finnybuddy/domain/user/repository/UserEntityRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserEntityRepository extends MongoRepository<UserEntity, Long> {
+public interface UserEntityRepository extends MongoRepository<UserEntity, String> {
     UserEntity findByEmail(String email);
 }
